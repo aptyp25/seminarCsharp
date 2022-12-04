78d15274bc27d3fa095263d7c66411ae4e20c07e
Artur Ilin <aptyp.25@mail.ru>
2022-12-04 19:47:50 +0300

    homework1.2

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 4: Напишите программу, которая принимает на вход 
+три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+ 
+if (c > max) max = c;
+
+Console.WriteLine($"max is {max} ");
+
