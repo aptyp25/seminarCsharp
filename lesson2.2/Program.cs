d6b7124aeae3ca9697c5ca7e9d8d002ca7ec99e3
Artur Ilin <aptyp.25@mail.ru>
2022-12-12 01:22:18 +0300

    delete second number

---
+++ lesson2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Напишите программу, которая выводит случайное трёхзначное 
+// число и удаляет вторую цифру этого числа.
+// 456 -> 46 782 -> 72 918 -> 98
+
+Random random = new Random();
+
+int number = random.Next(10, 1000);
+Console.WriteLine(number);
+
+int firstNumber = number / 100;
+int lastNumber = number % 10;
+
+int result = firstNumber*10 + lastNumber;
+
+
+Console.WriteLine(result);
+
