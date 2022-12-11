b3605685857fa766ed90f83fc64f789fd2276279
Artur Ilin <aptyp.25@mail.ru>
2022-12-12 00:10:18 +0300

    biggest number

---
+++ lesson2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//9. Напишите программу, которая выводит
+//случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Random random = new Random();
+
+int number = random.Next(10, 100);
+
+Console.WriteLine(number);
+
+int firstNumber = number / 10;
+int lastNumber = number % 10;
+
+if (firstNumber > lastNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(lastNumber);
+}
