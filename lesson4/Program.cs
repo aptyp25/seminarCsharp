1cb944b323a1536d51ff95bb8ad35fe56be142c9
Artur Ilin <aptyp.25@mail.ru>
2022-12-11 19:37:40 +0300

    quarter number

--- lesson4/Program.cs
+++ lesson4/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Напишите программу, которая на входе принимает число
+// и выдает его квадрат (число, умноженное на само себя)
+
+int number = int.Parse(Console.ReadLine());
+
+int quarter = number * number;
+
+Console.WriteLine(quarter);
+
