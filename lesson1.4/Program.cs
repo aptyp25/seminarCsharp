46be86c49f80408ac7e8e1e68b6ec8581ae67440
Artur Ilin <aptyp.25@mail.ru>
2022-12-11 22:19:49 +0300

    array from -number to +number

---
+++ lesson1.4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 2 -> " -2, -1, 0, 1, 2"
+
+int number = int.Parse(Console.ReadLine());
+
+int c;
+int b;
+
+if (number > 0){
+b = - number;
+c = number;
+}
+else{
+    b=number;
+    c=-number;
+}
+while (b <=c){
+    Console.WriteLine(b);
+    b++;
+}
