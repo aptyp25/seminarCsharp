5f6928cba63b8b36267504ea95a8f90d95a5706a
Artur Ilin <aptyp.25@mail.ru>
2022-12-12 10:01:35 +0300

    delim na 7 and 23

---
+++ lesson2.4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
+
+Console.WriteLine("Введите первое число, отличное от нуля");
+// int a = int.Parse(Console.ReadLine());
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+
+if (!isParsed)
+{
+    Console.WriteLine("То, что Вы ввели не есть целое число, запустите снова!");
+    return;
+}
+
+if (a % 7 == 0 & a%23 ==0)
+{
+    Console.WriteLine("Это число кратно одновременно 7ми и 23м");
+}
+else
+{
+    Console.WriteLine("Это число не кратно одновременно 7 и 23");
+}
+
